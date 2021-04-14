0c1496f756624cf4e3bfc0c4e4e3aaf58f7a8b50
Vinicius <castvn@pm.me>
2021-04-14 18:36:35 -0300

    Stores graph info in a SO

---
+++ Assets/Waypoints/Editor/WaypointTool.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+namespace Waypoints.Editor
+{
+    [EditorTool("Waypoints")]
+    public class WaypointTool : EditorTool
+    {
+        public override void OnToolGUI(EditorWindow window)
+        {
+            EditorGUI.BeginChangeCheck();
+
+            Vector3 position = Tools.handlePosition;
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Vector3 delta = position - Tools.handlePosition;
+
+                Undo.RecordObjects(Selection.transforms, "Move Platform");
+
+                foreach (var transform in Selection.transforms)
+                    transform.position += delta;
+            }
+
+        }
+    }
+}
